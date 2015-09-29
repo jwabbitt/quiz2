1f2aecfcd40a7db800b39e1c78e29d19ec8891ae
jwabbitt <jrabbitt1110@gmail.com>
2015-09-29 08:22:05 -0600

    Quiz 2 problems solved

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function arraySum(arr) {
+  var length = arr.length;
+  var sum = 0;
 
+  for (var i = 0; i < length; i++){
+    if (arr[i].length > 0) {
+      sum += arraySum(arr[i]);
+    } else {
+      sum += arr[i];
+    }
+  }
+  return sum;
 }
 
 module.exports = arraySum;
--- problems/longest-string.js
+++ problems/longest-string.js
@@ -1,7 +1,19 @@
 'use strict';
 
 function longestString(arr) {
+  var arrLength = arr.length;
+  var strLength = 0;
+  var longest;
 
+  for (var i = 0; i < arrLength; i++) {
+    if (typeof arr[i] === 'string') {
+      if (arr[i].length > strLength) {
+        var strLength = arr[i].length;
+        longest = arr[i];
+      }
+    }
+  }// end of for loop
+  return longest
 }
 
 module.exports = longestString;
